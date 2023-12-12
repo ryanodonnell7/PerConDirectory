6595da23a6b22ca85f5ffdf130c46052d4697979
ryanodonnell7 <51242459+ryanodonnell7@users.noreply.github.com>
2023-12-12 14:57:00 -0500

    added DispVars File

---
+++ dispVars.js
@@ -0,0 +1,49 @@
+workerGroups = [
+{group: 1, members: ['A', 'B', 'C', 'D', 'E', 'F','G', 'H', 'I', 'J', 'K', 'L','M', 'N', 'O', 'P', 'Q', 'R']},
+{group: 2, members: ['S', 'T', 'U', 'V', 'W', 'X','Y', 'Z', '0', '1', '2', '3','4', '5', '6', '7', '8', '9']}
+];
+
+
+
+
+
+
+var pracItems = ['butter','chandelier'];
+var pracOrder = [0, 1];
+var pracLeftPos = [170, 850];
+var pracTopPos  = [550, 220];
+var pracCat     = ['practice','practice'];
+
+
+grid_x = [1600]
+grid_y = [800]
+
+coords = {
+	x: [137.77,125.54,108.51,163.26,123.83,131.26,170.00,196.17,188.29,195.03,153.31,198.69,230.57,265.20,226.91,269.77,247.71,267.49,304.86,346.57,306.69,349.09,305.31,341.77,378.23,395.49,381.89,431.14,376.86,410.57,475.37,451.49,449.77,462.91,448.86,440.97,633.77,627.94,578.91,648.74,638.11,629.54,670.57,708.86,682.00,703.83,700.29,722.11,739.37,782.91,747.60,782.23,755.14,780.86,811.83,854.00,814.11,862.46,820.06,846.91,903.49,894.69,896.17,927.14,887.94,931.71,938.46,958.00,922.46,984.51,966.11,1013.77,1159.71,1201.43,1137.77,1166.23,1152.40,1166.23,1198.80,1249.20,1214.80,1251.03,1196.97,1236.40,1264.40,1309.09,1275.37,1302.46,1266.00,1295.60,1337.77,1387.94,1342.34,1372.17,1332.51,1374.00,1422.11,1473.43,1414.80,1462.00,1395.83,1459.71,1442.00,1550.00,1467.60,1469.54,1501.20,1539.03,1288.62,819.99,1436.66,485.69,494.28,503.59,773.75,918.46,686.06,568.70,574.75,571.70,1523.45,52.55,1153.22,1004.41,1027.24,1014.54,872.72,706.65,670.93,1108.01,1089.74,1100.37,87.59,485.44,188.67],
+	y: [128.91,218.51,330.00,532.39,634.72,744.68,50.00,162.51,272.47,482.50,580.25,680.54,118.22,217.49,319.82,527.81,624.54,744.68,80.55,166.58,278.58,457.05,582.79,672.90,119.75,216.47,350.36,532.39,631.66,729.41,50.00,166.07,278.58,459.59,584.32,666.79,176.13,273.87,370.09,540.76,660.40,750.00,97.73,223.98,338.02,498.00,609.49,695.02,173.07,272.35,347.18,568.25,656.84,747.45,133.36,217.36,315.11,495.45,603.89,708.76,156.27,272.85,352.27,540.76,650.73,727.09,128.27,223.98,338.02,482.73,608.98,688.91,123.82,220.04,296.91,480.31,587.22,669.18,57.64,172.69,285.71,445.18,527.65,637.62,120.76,224.11,312.18,478.78,584.67,681.91,50.00,170.15,253.64,409.55,536.82,614.71,132.98,227.67,309.64,475.73,586.71,659.00,57.64,171.16,255.16,447.73,534.27,617.76,375.57,386.96,371.97,735.83,648.27,553.37,474.05,52.28,729.65,222.07,67.97,145.77,388.22,482.28,411.89,98.28,267.62,173.43,553.56,139.23,50.10,739.06,556.24,662.58,389.91,384.38,388.50],
+	quad: [1,1,1,2,2,2,1,1,1,2,2,2,1,1,1,2,2,2,1,1,1,2,2,2,1,1,1,2,2,2,1,1,1,2,2,2,3,3,3,4,4,4,3,3,3,4,4,4,3,3,3,4,4,4,3,3,3,4,4,4,3,3,3,4,4,4,3,3,3,4,4,4,5,5,5,6,6,6,5,5,5,6,6,6,5,5,5,6,6,6,5,5,5,6,6,6,5,5,5,6,6,6,5,5,5,6,6,6,12,12,12,13,13,13,16,16,16,42,42,42,43,43,43,46,46,46,52,52,52,53,53,53,56,56,56]
+};
+
+cluster_center_x = [285.51746, 295.43492, 786.95238, 811.19365, 1324.9206, 1329.9619]
+cluster_center_y = [195.8263, 603.8333, 244.996, 624.0848, 191.2162, 554.1556]
+
+countBal = [[1,  4,  5], [2,  3,  6]]
+
+colors = ['blue', 'green', 'brown']
+
+categories = ['birds', 'vehicles', 'sports']
+
+birds = ['Abird15.jpg', 'Abird16.jpg', 'Abird3.jpg', 'Abird4.jpg', 'Abird5.jpg', 'Abird6.jpg', 'Abird7.jpg', 'Abird8.jpg', 'aBird1.jpg', 'aBird10.jpg', 'aBird11.jpg', 'aBird12.jpg', 'aBird13.jpg', 'aBird17.jpg', 'aBird18.jpg', 'aBird2.jpg', 'aBird9.jpg', 'abird14.jpg']
+
+blue = ['41tExEsn-gL._AA262_.jpg', 'ABUTTERF7.jpg', 'AFOOTST11.jpg', 'AOVENMIT5.jpg', 'APERFBOT9.jpg', 'Aballoon104.jpg', 'Aguitar78.jpg', 'Alaundrybask2.jpg', 'Ashark7.jpg', 'Table_Fan.jpg', 'WLJ36834NG_1_1_400.JPG', 'blueSign.jpg', 'blueberriespack.jpg', 'boot.jpg', 'drum04.jpg', 'glass02a.jpg', 'jellyfish.jpg', 'tshirt.jpg']
+
+brown = ['26419975.thl.jpg', '8027656.thl.jpg', 'AMSHOES6.jpg', 'AWOODBIN.jpg', 'accordion01.jpg', 'acf66m.jpg', 'apron.jpg', 'bison.jpg', 'breadslice.jpg', 'coat.jpg', 'coconut.jpg', 'curtain.jpg', 'horse.jpg', 'nightstand.jpg', 'paintbrush.jpg', 'stingray.jpg', 'teapot.jpg', 'violin.jpg']
+
+critical_items = ['blueBirds1.jpg', 'blueBirds2.jpg', 'blueBirds3.jpg', 'blueSports1.jpg', 'blueSports2.jpg', 'blueSports3.jpg', 'blueVehicles1.jpg', 'blueVehicles2.jpg', 'blueVehicles3.jpg', 'brownBirds1.JPG', 'brownBirds2.jpg', 'brownBirds3.jpg', 'brownSports1.jpg', 'brownSports2.jpg', 'brownSports3.jpg', 'brownVehicles1.jpg', 'brownVehicles2.jpg', 'brownVehicles3.jpg', 'greenBirds1.jpg', 'greenBirds2.jpg', 'greenBirds3.jpg', 'greenSports1.jpg', 'greenSports2.jpg', 'greenSports3.jpg', 'greenVehicles1.jpg', 'greenVehicles2.jpg', 'greenVehicles3.jpg']
+
+green = ['AMINITRE7.jpg', 'ATAPEDIS7.jpg', 'AWREATH9.jpg', 'Agardeningpai.jpg', 'Agreenplastic.jpg', 'CanadaDryGingerAleL.jpg', 'DSCN1248-sm.jpg', 'bowl02c.jpg', 'boxcutter02a.jpg', 'broccoli01c.jpg', 'caterpillar.jpg', 'crayon.jpg', 'frog.jpg', 'gecko.jpg', 'highlighter06.jpg', 'lime.jpg', 'overalls.jpg', 'pepper05b.jpg']
+
+sports = ['8ball.jpg', 'Model08Purple.JPG', 'airhockeypaddle01a.jpg', 'badmintonracket.jpg', 'baseball01b.jpg', 'baseballbat.jpg', 'basketball01.jpg', 'birdie.jpg', 'bowlingBall.jpg', 'bowlingpin.jpg', 'frisbee.jpg', 'golfBall.jpg', 'hockey.jpg', 'hoop.jpg', 'paddle.jpg', 'soccerball.jpg', 'tennisRacket.jpg', 'volleyball.jpg']
+
+vehicles = ['Akayak.jpg', 'bike.jpg', 'bus.jpg', 'car.jpg', 'ferry.jpg', 'firetruck.jpg', 'golfcart.jpg', 'helicopter.jpg', 'jeep.jpg', 'lawnmowertractor.jpg', 'motorboat02.jpg', 'motorcycle.jpg', 'schoolbus.jpg', 'tank.jpg', 'tractor.jpg', 'train.jpg', 'truck.jpg', 'van.jpg']
+
